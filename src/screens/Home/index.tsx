355462473605442cb97e7ef5bc48b995cf7dffb8
BarbaraEspenchidt <barbaraespenchidt@hotmail.com>
2022-07-03 17:16:47 -0300

    Home
    Estilizando a home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+
+<View>
+
+</View>
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export const styles= Stylesheet.create ({
+    container:{
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingTop: 60,
+        paddingHorizontal: 30
+    }
+
+
+})
