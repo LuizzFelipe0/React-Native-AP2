f77735511f7d29596921ec6ad7f9e1463c99ef25
BarbaraEspenchidt <barbaraespenchidt@hotmail.com>
2022-07-07 12:18:21 -0300

    SplashScreen

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Login } from './src/screens/Login';
-import { MarvelList } from './src/screens/MarvelList';
-import { ComicList } from './src/screens/ComicList';
-import { Registration } from './src/screens/Registration';
+import React, {useEffect, useState} from 'react';
 import { Router } from './src/routes';
+<<<<<<< Updated upstream
+
+=======
+import { UserInfoProvider } from './src/contexts/UserInfoProvider';
+import Splash from "./src/assets/images/splash.png";
+import AnimatedSplash from "react-native-animated-splash-screen";
 
 
-export default function App() {
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+        setIsLoaded(true);
+    }, 2000)
+  });
+>>>>>>> Stashed changes
+
   return (
+<<<<<<< Updated upstream
     <Router/>
     // <Registration/>
+=======
+    <>
+    <AnimatedSplash>
+      isLoaded={isLoaded}
+      logoImage={Splash}
+      backgroundColor={"#fff"}
+      logoHeigh={150}
+      logoWidth={150}
+    </AnimatedSplash>
+    
+
+
+    <UserInfoProvider>
+      <Router/>
+    </UserInfoProvider>
+
+    </>
+>>>>>>> Stashed changes
   )
 }
